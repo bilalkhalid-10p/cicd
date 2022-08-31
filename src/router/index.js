f6eaa3ac1f584f1ab9496608caf768e94904eb99
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-08-31 12:37:25 +0500

    resolved issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
-import { Vue } from 'vue'
-import Router from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import helloWorld from '@/components/HelloWorld'
 import list from '@/components/List'
 
-Vue.use(Router)
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: helloWorld
+    },
+    {
+        path: '/list',
+        name: 'list',
+        component: list
+    }
+]
 
-export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: helloWorld
-        },
-        {
-            path: '/list',
-            name: 'list',
-            component: list
-        }
-    ]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
 })
+
+export default router
