2d9f1a64fb13966c846b6e910c1e9bd2a782bfbf
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-09-01 16:25:51 +0500

    added table

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,22 @@
   <div>
     <b-card>
       <h1>Users Data</h1>
+      <table class="table table-bordered table-striped table-sm">
+        <tr>
+          <th>Employee First Name</th>
+          <th>Employee Last Name</th>
+          <th>Manager First Name</th>
+          <th>Manager Last Name</th>
+          <th>Action</th>
+        </tr>
+        <tr v-for="(item, index) in list" :key="index">
+          <td>{{item.Employee_first_name}}</td>
+          <td>{{item.Employee_last_name}}</td>
+          <td>{{item.Manager_first_name}}</td>
+          <td>{{item.Manager_first_name}}</td>
+          <td></td>
+        </tr>
+      </table>
       <b-table striped hover :items="list"></b-table>
     </b-card>
   </div>
