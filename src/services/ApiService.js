68b65eaa1adf00f430b457c063fdbf5d076e4b6c
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-09-05 16:26:36 +0500

    updated create user

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 const instance = Axios.create({
     baseURL: 'https://desolate-mountain-37815.herokuapp.com',
     headers: {
+        'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
 });
