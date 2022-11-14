e1c0dc09af15c485f20d480f81b720c568cc384a
Bilal.Khalid <bilal.khalid@10pearls.com>
2022-11-14 14:21:37 +0500

    added docker file and updated backend url

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const instance = Axios.create({
-    baseURL: 'https://desolate-mountain-37815.herokuapp.com/',
+    //baseURL: 'https://desolate-mountain-37815.herokuapp.com/',
+    baseURL: 'http://localhost:8082/',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
